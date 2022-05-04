78f4a924d6be657537a8dd85e6e3ce908ef842ef
victorterente <73474868+victorterente@users.noreply.github.com>
2022-05-04 12:29:07 +0100

    teste

--- models/PessoaModels.js
+++ models/PessoaModels.js
@@ -3,14 +3,14 @@ const express = require('express');
 const {log} = require("debug");
 const app = express();
 
-module.exports.getPessoas = async function() {
-    console.log("[PessoaModels.getPessoas] id = ")
+module.exports.getPessoas = async function(id) {
+    console.log("[PessoaModels.getPessoas] id = " + JSON.stringify(id))
     try {
-        let sql = "select * from pessoa";
-        let result = await client.query(sql);
+        let sql = "select * from pessoa where pessoa_id = &1";
+        let result = await client.query(sql, [id]);
         let pessoa = result.rows;
         if (pessoa.lenght > 0){
-            console.log("[PessoaModels.getPessoas] pessoa = " );
+            console.log("[PessoaModels.getPessoas] pessoa = " + JSON.stringify(pessoa[0]) );
             return { status: 200, data: pessoa[0] };
         } else {
             return { status: 200, data: {msg:"user not found." }};
