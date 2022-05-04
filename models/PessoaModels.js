3d3df29f0b8cb17070956f7e2c07bda1ea1e7891
victorterente <73474868+victorterente@users.noreply.github.com>
2022-05-04 12:41:50 +0100

    teste

--- models/PessoaModels.js
+++ models/PessoaModels.js
@@ -8,13 +8,14 @@ module.exports.getPessoas = async function(id) {
     try {
         let sql = 'select * from pessoa where pessoa_id = $1';
         let result = await client.query(sql, [id]);
-        let pessoa = result.rows;
-        if (pessoa.lenght > 0){
-            console.log("[PessoaModels.getPessoas] pessoa = " + JSON.stringify(pessoa[0]) );
-            return { status: 200, data: pessoa[0] };
-        } else {
-            return { status: 200, data: {msg:"user not found." }};
-        }
+        let pessoa = result;
+        // if (pessoa.lenght > 0){
+        //     console.log("[PessoaModels.getPessoas] pessoa = " + JSON.stringify(pessoa[0]) );
+        //     return { status: 200, data: pessoa[0] };
+        // } else {
+        //     return { status: 200, data: {msg:"user not found." }};
+        // }
+        return { status: 200, data: pessoa };
     } catch (err) {
         console.log(err);
         return { status: 500, data: err };
