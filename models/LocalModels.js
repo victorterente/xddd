19db66c7607f8ff3b05e5f79c90125d0a22cc5d2
victorterente <73474868+victorterente@users.noreply.github.com>
2022-05-06 15:20:56 +0100

    mensagem de commit

--- models/LocalModels.js
+++ models/LocalModels.js
@@ -15,3 +15,25 @@ module.exports.getAlllocais = async function () {
         return { status: 500, result: err };
     }
 };
+
+module.exports.getLocais = async function(id) {
+    console.log("[LocalModels.getLocais] id = " + JSON.stringify(id))
+    try {
+        let sql = 'select * from local where local_id = $1';
+        let result = await client.query(sql, [id]);
+        let local = result.rows;
+        // if (pessoa.lenght > 0){
+        //     console.log("[PessoaModels.getPessoas] pessoa = " + JSON.stringify(pessoa[0]) );
+        //     return { status: 200, data: pessoa[0] };
+        // } else {
+        //     return { status: 200, data: {msg:"user not found." }};
+        // }
+        return {status: 200, data: local};
+    } catch (err) {
+        console.log(err);
+        return {status: 500, data: err};
+    }
+
+
+
+}
--- routes/localroutes.js
+++ routes/localroutes.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var LocalModels = require('../models/LocalModels');
+const PessoaModels = require("../models/PessoaModels");
 
 
 
@@ -9,6 +10,13 @@ router.get("/", async function (req, res, next) {
     res.status(result.status).send(result.result.rows);
 });
 
+router.get('/:id(\\d+)', async function(req, res, next) {
+    let id = req.params.id
+    console.log("[localroutes] user with id:" + id);
+    let result = await LocalModels.getLocais(id);
+    res.status(result.status).send(result.data);
+
+});
 
 module.exports = router;
 
