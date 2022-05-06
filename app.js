cf3dbcfc222e876c2f53823a382be5261258a5f3
victorterente <73474868+victorterente@users.noreply.github.com>
2022-05-06 15:01:20 +0100

    mensagem de commit

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var pessoaRouter = require('./routes/pessoaroutes');
-var tipolocalRouter = require('./routes/tipolocaisroutes');
-var localRouter = require('./routes/locaisroutes');
+
 var app = express();
 
 app.use(logger('dev'));
@@ -19,6 +18,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/pessoa', pessoaRouter);
-app.use('/tipolocal', tipolocalRouter);
-app.use('/local', localRouter);
+
 module.exports = app;
