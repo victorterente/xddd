d7c7c29dc23b6f326a482300f24cb712bc4a7f02
victorterente <73474868+victorterente@users.noreply.github.com>
2022-05-11 09:12:12 +0100

    mensagem de commit

--- models/EventoModels.js
+++ models/EventoModels.js
@@ -14,3 +14,27 @@ module.exports.getAllEventos = async function () {
         return { status: 500, result: err };
     }
 };
+module.exports.registerEvento = async function (newevento) {
+    try {
+        let nome = newevento.nome;
+        let morada = newevento.morada;
+        let data = newevento.data;
+        let cap = newevento.cap;
+        let geo = newevento.geo;
+        let desc = newpessoa.desc;
+
+        //var sql = "SELECT * FROM pessoa WHERE pessoa_email =?";
+        //  let result = await pool.query(sql, [email]);
+        //   if (result.length > 0)
+        //      return { status: 401, result: { msg: "Já está registado" } };
+        //  else {
+        var sql = 'INSERT INTO pessoa (evento_nome, evento_morada, evento_data, evento_cap, evento_desc,evento_geo) VALUES ($1,$2,$3,$4,$5,$6,$7)';
+        let result = await client.query(sql,[ nome, morada, data , cap, desc, geo ])
+        let pessoa = result.rows;
+        return { status: 200, result: { msg: "registado com sucesso" }, data : pessoa };;
+        //   }
+    } catch (err) {
+        console.log(err);
+        return { status: 500, result: err };
+    }
+};
--- routes/eventoroutes.js
+++ routes/eventoroutes.js
@@ -4,10 +4,15 @@ var EventoModels = require('../models/EventoModels');
 
 
 
+
 router.get("/", async function (req, res, next) {
     let result = await EventoModels.getAllEventos();
     res.status(result.status).send(result.result.rows);
 });
-
+router.post('/new',async function(req, res, next) {
+    let body = req.body
+    let result = await EventoModels.registerEvento(body);
+    res.status(result.status).send(result.data);
+});
 
 module.exports = router;
