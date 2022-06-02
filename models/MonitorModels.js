c1628a6c9cf27ca2d87685cb78129ea2f05c1861
victorterente <73474868+victorterente@users.noreply.github.com>
2022-06-02 19:22:59 +0100

    mensagem de commit

--- models/MonitorModels.js
+++ models/MonitorModels.js
@@ -6,10 +6,10 @@ const app = express();
 
 module.exports.getAllMonitor = async function () {
     try {
-        let sql = "select * from pessoa inner join admin on pessoa_id = monitor_pessoa_id";
+        let sql = "select * from pessoa inner join monitor on pessoa_id = monitor_pessoa_id";
         let result = await client.query(sql);
-        let admin = result;
-        return { status: 200, result: admin };
+        let monitor = result;
+        return { status: 200, result: monitor };
     } catch (err) {
         console.log(err);
         return { status: 500, result: err };
