62564be0a050475a7b4d34640ec23c51bdb4b6e7
victorterente <73474868+victorterente@users.noreply.github.com>
2022-06-03 17:51:17 +0100

    mensagem de commit

--- models/EventoModels.js
+++ models/EventoModels.js
@@ -22,14 +22,15 @@ module.exports.registerEvento = async function (newevento) {
         let cap = newevento.cap;
         let geo = newevento.geo;
         let desc = newevento.desc;
+        let img = newevento.img;
 
         //var sql = "SELECT * FROM pessoa WHERE pessoa_email =?";
         //  let result = await pool.query(sql, [email]);
         //   if (result.length > 0)
         //      return { status: 401, result: { msg: "Já está registado" } };
         //  else {
-        var sql = 'INSERT INTO evento (evento_nome, evento_morada, evento_data, evento_cap, evento_desc, evento_geo) VALUES ($1,$2,$3,$4,$5,$6)';
-        let result = await client.query(sql,[ nome, morada, data , cap, desc, geo ])
+        var sql = 'INSERT INTO evento (evento_nome, evento_morada, evento_data, evento_cap, evento_desc, evento_geo, evento_img) VALUES ($1,$2,$3,$4,$5,$6,$7)';
+        let result = await client.query(sql,[ nome, morada, data , cap, desc, geo, img ])
         let pessoa = result.rows;
         return { status: 200, result: { msg: "registado com sucesso" }, data : pessoa };;
         //   }
