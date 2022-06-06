0a5bbc9672d0ef62e6b9338e9b0e8d3593c7ca2d
victorterente <73474868+victorterente@users.noreply.github.com>
2022-06-06 21:26:49 +0100

    mensagem de commit

--- routes/inscricaoroutes.js
+++ routes/inscricaoroutes.js
@@ -21,7 +21,7 @@ router.get('/:id(\\d+)', async function(req, res, next) {
 router.post('/reservar', async function(req, res, next) {
     let eventoId = req.body.eventoId;
     let pessoaId = req.body.pessoaId;
-    console.log("Reserva feita with id "+PessoaId);
+    console.log("Reserva feita with id "+pessoaId);
     let result = await ReservaModels.reservaPessoa(eventoId, pessoaId);
     res.status(result.status).send(result.result.rows);
 });
