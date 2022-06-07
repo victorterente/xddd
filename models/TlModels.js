9f1b0b7ce4c666a0965b5657a82e054ab32baf71
victorterente <73474868+victorterente@users.noreply.github.com>
2022-06-07 19:31:09 +0100

    mensagem de commit

--- models/TlModels.js
+++ models/TlModels.js
@@ -3,6 +3,19 @@ const express = require('express');
 const {log} = require("debug");
 const app = express();
 
+
+module.exports.localtipo = async function (localId , tipolocalId) {
+    try {
+            let sql = "INSERT INTO tl (tl_local_id,tl_tipo_id) values ($1,$2)";
+            let result = await client.query(sql, [localId , tipolocalId]);
+
+            return { status: 200, result: { msg: "Inscrição efectuada com sucesso" } };
+
+    } catch (err) {
+        console.log(err);
+        return { status: 500, result: err };
+    }
+}
 module.exports.getTl = async function(id) {
     console.log("[TlModels.getTl] id = " + JSON.stringify(id))
     try {
--- routes/tlroutes.js
+++ routes/tlroutes.js
@@ -4,11 +4,19 @@ var TlModels = require('../models/TlModels');
 
 
 
+
 router.get('/:id(\\d+)', async function(req, res, next) {
     let id = req.params.id
     console.log("[tlroutes] user with id:" + id);
     let result = await TlModels.getTl(id);
     res.status(result.status).send(result.data);
 
+});
+router.post('/tipo', async function(req, res, next) {
+    let localId = req.body.localId;
+    let tipolocalId = req.body.tipolocalId;
+
+    let result = await TlModels.localtipo(localId, tipolocalId);
+    res.status(result.status).send(result.result.rows);
 });
 module.exports = router;
